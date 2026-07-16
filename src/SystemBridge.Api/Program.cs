46ba1ee2fe064b38f5229e75324a90798ada8964
Matija Jarc <matija_jarc@hotmail.com>
2026-07-16 17:58:02 +0200

    Added a client UI for ordering and updating the inventory

--- src/SystemBridge.Api/Program.cs
+++ src/SystemBridge.Api/Program.cs
@@ -8,6 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost", policy =>
+    {
+        policy.WithOrigins("http://127.0.0.1:8000", "http://localhost:8000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 builder.Services.AddDbContext<SystemBridgeDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=systembridge.db"));
 builder.Services.AddScoped<IInventorySyncService, InventorySyncService>();
@@ -39,6 +48,7 @@ using (var scope = app.Services.CreateScope())
     db.SaveChanges();
 }
 
+app.UseCors("AllowLocalhost");
 app.UseSwagger();
 app.UseSwaggerUI();
 app.MapControllers();
